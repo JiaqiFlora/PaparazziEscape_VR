9e6f9125ecdb414e9ae76b5f7b572ec3d3f13b9b
Flora <jiaqigan.kiki@gmail.com>
2023-05-26 23:52:39 -0400

    fix video scene direction problem

--- Assets/Scripts/VideoSceneControl.cs
+++ Assets/Scripts/VideoSceneControl.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class VideoSceneControl : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    public Transform userTransform;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,10 @@ public class VideoSceneControl : MonoBehaviour
 
     IEnumerator StartEndingVideo()
     {
+        // just in case user face other directions!
+        player.transform.position = userTransform.position;
+        player.transform.rotation = userTransform.rotation;
+
         FadeScreen.instance.FadeIn(4f);
         //yield return new WaitForSeconds(1f);
         yield return null;
